e002258fbe315c2feaec0be25fc9ce1ae9873f0f
Riffat <riffat.builds@gmail.com>
2026-06-06 14:14:40 +0500

    Code restructured

--- firstcode.js
+++ firstcode.js
@@ -1,21 +1,26 @@
-"use strict"
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("mybutton").onclick = function () {
+    document.getElementById("myheading").innerHTML =
+      "<b style='color:red'> Welcome Riffat!</b>";
+  };
+});
 function addnum(a, b) {
-    let result = a + b;
-    return result;
+  let result = a + b;
+  return result;
 }
 console.log(addnum(5, 3));
 console.log(addnum(10, 20));
 console.log(addnum(50, 200));
-
 const myname = "Riffat";
 const myjob = "Web developer";
 function introduce(name, job) {
-    return "My name is " + name + " and I am a " + job;
+  return "My name is " + name + " and I am a " + job;
 }
 console.log(introduce(myname, myjob));
 
 //long calculate age
-
 /*const year = 2026;
 const birthday = 1990;
 function calcage(currentyear, birthyear) {
@@ -26,8 +31,8 @@ function calcage(currentyear, birthyear) {
 console.log(calcage(year, birthday));
 */
 function calcage(birthyear) {
-    let age = 2026 - birthyear;
-    return "your age is " + age + " years";
+  let age = 2026 - birthyear;
+  return "your age is " + age + " years";
 }
 console.log(calcage(1990));
 //Student report card
@@ -46,14 +51,10 @@ console.log(result(marks, totalmarks));*/
 const marks = 95;
 const studentName = "Riffat";
 let grade = null;
-if (marks > 90)
-    grade = "A Grade";
-else if (marks > 80)
-    grade = "B Grade";
-else if (marks > 70)
-    grade = "C Grade";
-else if (marks > 50)
-    grade = "D Grade";
+if (marks > 90) grade = "A Grade";
+else if (marks > 80) grade = "B Grade";
+else if (marks > 70) grade = "C Grade";
+else if (marks > 50) grade = "D Grade";
 
 console.log(`${studentName} got ${grade}`);
 const ispass = marks >= 50 ? "Pass" : "fail";
